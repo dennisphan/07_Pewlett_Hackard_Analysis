946ca4d97de30d46ae1cb72f7e0484d2b9f40111
dennisphan <denis.phan262@gmail.com>
2021-07-21 12:35:43 -0600

    Update schema.sql

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -17,23 +17,34 @@ CREATE TABLE employees (
     UNIQUE (emp_no)
 );
 
-CREATE TABLE managers (
-dept_no VARCHAR(4) NOT NULL,
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  	emp_no INT NOT NULL,
+  	salary INT NOT NULL,
+  	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4),
+	from_date DATE,
+	to_date DATE,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no), 
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no), 	
+	FOREIGN KEY (emp_no) REFERENCES salaries(emp_no)
 );
 
 CREATE TABLE titles (
@@ -41,21 +52,16 @@ CREATE TABLE titles (
 	title VARCHAR,
 	from_date DATE,
 	to_date DATE,
-	PRIMARY KEY (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	PRIMARY KEY (emp_no, title),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no), 
+	FOREIGN KEY (emp_no) REFERENCES salaries(emp_no)
 );
 
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
-	from_date DATE,
-	to_date DATE,
-	PRIMARY KEY (emp_no, dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no), 	
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
-);
+DROP TABLE titles;
 
 
+
+-- DROP TABLE departments, employees, dept_emp, managers, salaries, titles
 
 
 
